cf3b651fc3f242586979fcca4a7331082b80835d
poramesh <poojajramesh@gmail.com>
2023-04-04 15:10:08 +0100

    login authentication

--- login.php
+++ login.php
@@ -1,24 +1,25 @@
 <?php
 include("connection.php");
    if(isset($_POST['submit'])){
-	   session_start();
+	session_start();
 	$username = $_POST['username'];
 	$_SESSION['username']=$username;
 	$pswrd = $_POST['pswrd'];
     $sql = "select * from login where username = '$username' and password = '$pswrd'";
-	 $result = mysqli_query($conn, $sql);
+	$result = mysqli_query($conn, $sql);
+    //$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $count = mysqli_num_rows($result);
+
 	$sql1 = "select studentid from login where username = '$username' and password = '$pswrd'";
-    
 	$result1 = mysqli_query($conn, $sql1);
-	 $row1 = mysqli_fetch_row($result1);
+	$row1 = mysqli_fetch_row($result1);
 	$studentid= $row1[0];
 	 echo "<br>";
 	 $_SESSION['studentid']=$row1[0];
 	 echo $studentid;
 
 
-    $count = mysqli_num_rows($result);
-    if($count=1){
+    if($count==1){
         header("Location:module.html");
     }
     
