26e599bbfecb524f0a20b92bb03b313b59c63754
TejGitH <128676595+TejGitH@users.noreply.github.com>
2023-04-04 13:03:38 +0100

    Add files via upload

--- insert_user.php
+++ insert_user.php
@@ -1,48 +1,27 @@
-<?php session_start();?>
 <?php
-	$fname = $_REQUEST["firstname"];
-	$lname = $_REQUEST["lastname"];
-	$dob = $_REQUEST["dob"];
-	$city = $_REQUEST["city"];
-	$file = $_FILES["image"];
-	$gender = $_REQUEST["gender"];
-	$mobile = $_REQUEST["mobile"];
-	$email = $_REQUEST["email"];
+include("connection.php");
 	
-	$size = $file["size"]/1024;
-	$type =	$file["type"];
-	$source = $file["tmp_name"];
-	$destination = "../image_store/users/$email.jpg";
-	
-	if($size<12 && $type=="image/jpeg"){
-		move_uploaded_file($source,$destination);
-		//upload data
-		
-		$conn = mysqli_connect("localhost","root","","project");
-		//db connection
-		
-		if(!$conn){
-			die("Connection failed");
-		}
-		
-		$username = $fname.".".$lname;
-		$password = $fname."@123";
-		$usertype = "client";
-		
-		$query = "insert into users values('','$username','$password','$fname','$lname','$dob','$city','$email','$gender','$destination','$usertype')";
-		echo $query;
-		$result = mysqli_query($conn,$query);
-		
-		if($result){
-			$_SESSION["msg"] = "Login was succesful <br/><br/>Username : Your Email <br/>Password : firstname@123";
-			$_SESSION["insertid"] = mysqli_insert_id($conn);
-			header("Location:set_sec_question.php");
-		}else{
-			$_SESSION["msg"] = "Registration failed";
-			header("Location:register.php");
-		}
+	session_start();
+	echo "<br>";
+	echo "isset successful";
+	echo "<br>";
+	$FirstName = $_POST["firstname"];
+	$LastName = $_POST["lastname"];
+	$Email = $_POST["email"];
+	$Username = $_POST["username"];
+	$Password = $_POST["password"];
+	$usertype = $_POST["usertype"];
+
+	$query = "insert into login(Username, Password, usertype, FirstName, LastName, Email) values('$Username','$Password','$usertype','$FirstName','$LastName','$Email')";
+	echo $query;
+	$result = mysqli_query($conn,$query);
+
+	if($result){
+		$_SESSION["msg"] = "Signup was succesful <br/><br/>Username : Your Email <br/>Password : firstname@123";
+		$_SESSION["insertid"] = mysqli_insert_id($conn);
+		header("Location:loginPage.html");
 	}else{
-		$_SESSION["msg"] = "use max 10kb and jpeg image only";
-		header("Location:register.php");
+		$_SESSION["msg"] = "Registration failed";
+		header("Location:registerPage.html");
 	}
 ?>
